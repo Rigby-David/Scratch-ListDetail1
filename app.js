06fffca204f2b376460f382696d34f56edaeb949
David <Rigby335@hotmail.com>
2022-05-10 11:41:09 -0700

    copied app.js from spotlight and imported

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions and grab DOM elements
-
+import { tools } from './tools.js';
+import { renderTool } from './render-utils.js';
 // let state
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const toolListEl = document.getElementById('tools');
+for (let tool of tools) {
+    const toolDiv = renderTool(tool);
+    toolListEl.append(toolDiv);
+}
--- detail-page/detail.js
+++ detail-page/detail.js
@@ -1,3 +1,5 @@
 const toolName = document.getElementById('tool-name');
 const toolImage = document.getElementById('tool-image');
-const toolInfo = document.getElementById('tool-info');
+const toolDig = document.getElementById('tool-dig');
+const toolDurability = document.getElementById('tool-durability');
+const toolPerks = document.getElementById('tool-perks');
