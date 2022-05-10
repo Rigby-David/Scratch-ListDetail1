290fe4a9683002706c6212a336dc1f6fd26c4384
David <Rigby335@hotmail.com>
2022-05-10 11:58:39 -0700

    test running, not RED yet

--- detail-page/detail.js
+++ detail-page/detail.js
@@ -1,5 +1,16 @@
+import { tools } from '../tools.js';
+import { findById } from '../utils.js';
+
 const toolName = document.getElementById('tool-name');
 const toolImage = document.getElementById('tool-image');
 const toolDig = document.getElementById('tool-dig');
 const toolDurability = document.getElementById('tool-durability');
 const toolPerks = document.getElementById('tool-perks');
+
+const params = new URLSearchParams(window.location.search);
+const tool = findById(params.get('id'), tools);
+toolName.textContent = tool.name;
+toolImage.src = `../assets/${tool.image}.jpg`;
+toolDig.textContent = tool.dig;
+toolDurability.textContent = tool.durability;
+toolPerks.textContent = tool.perks;
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,27 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderTool } from '../render-utils';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderTool renders a div with tool details', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="tool"><img src="./assets/shovel.jpg"><h1>Spade</h1><p>The Spade has a digging factor of 7 and a durability factor of 9</p><h2>Perks</h2><ul><li>Easy to use</li><li>Versatile</li><li>Decent at killing zombies</li></ul></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const Spade = {
+        name: 'Spade',
+        dig: 7, 
+        durability: 9, 
+        image: './assets/shovel.jpg', 
+        perks: ['Easy to use', 'Versatile', 'Decent at killing zombies']
+    };
+    const actual = renderTool(Spade);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
