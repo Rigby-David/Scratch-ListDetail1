3a796ae87df2edf3d23d0616117de5983b199586
David <Rigby335@hotmail.com>
2022-05-10 11:45:04 -0700

    copied basic home.css

--- app.js
+++ app.js
@@ -2,13 +2,12 @@
 import { tools } from './tools.js';
 import { renderTool } from './render-utils.js';
 // let state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 const toolListEl = document.getElementById('tools');
 for (let tool of tools) {
     const toolDiv = renderTool(tool);
     toolListEl.append(toolDiv);
 }
+// set event listeners 
+  // get user input
+  // use user input to update state 
+  // update DOM to reflect the new state
