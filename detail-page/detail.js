01f376b0d22c8d521b7fb58414954877e03dbf61
David <Rigby335@hotmail.com>
2022-05-10 14:29:06 -0700

    restructured detail.js

--- detail-page/detail.js
+++ detail-page/detail.js
@@ -1,16 +1,21 @@
 import { tools } from '../tools.js';
 import { findById } from '../render-data-utils.js';
 
-const toolName = document.getElementById('tool-name');
-const toolImage = document.getElementById('tool-image');
-const toolDig = document.getElementById('tool-dig');
-const toolDurability = document.getElementById('tool-durability');
-const toolPerks = document.getElementById('tool-perks');
+const imageEl = document.getElementById('image');
+const toolName = document.getElementById('toolName');
+const toolDig = document.getElementById('dig');
+const toolDurability = document.getElementById('durability');
 
 const params = new URLSearchParams(window.location.search);
 const tool = findById(params.get('id'), tools);
+// toolName.textContent = tool.name;
+// toolImage.src = `../assets/${tool.image}.jpg`;
+// toolDig.textContent = tool.dig;
+// toolDurability.textContent = tool.durability;
+// toolPerks.textContent = tool.perks;
+
+imageEl.src = `.${tool.image}`;
+
 toolName.textContent = tool.name;
-toolImage.src = `../assets/${tool.image}.jpg`;
 toolDig.textContent = tool.dig;
 toolDurability.textContent = tool.durability;
-toolPerks.textContent = tool.perks;
